70569518097ee099db93bc0c90883833b3f847c8
Rahul Singh <singh.rahul.mailbox@gmail.com>
2015-12-11 17:00:03 +0530

    Refactor

--- tests/ProgressBar.test.js
+++ tests/ProgressBar.test.js
@@ -1,84 +1,83 @@
+
 'use strict';
 
-    Ractive.DEBUG = false;
+Ractive.DEBUG = false;
 
-    var div1 = document.createElement("div");
-    div1.setAttribute("id", "container1");
-    document.body.appendChild(div1);
+var pb1;
 
-    var max = 100;
-    var progress = 0;
-    
-    var pb1;
+var div1 = document.createElement("div");
+div1.setAttribute("id", "container1");
+document.body.appendChild(div1);
 
-    Ractive.load({
-        ProgressBar: 'base/app/components/progressBar/ProgressBar.html',
-    }).then(function(components){
+var max = 100;
+var progress = 0;
 
-        pb1 = new components.ProgressBar({
+Ractive.load({
+    ProgressBar: 'base/app/components/progressBar/ProgressBar.html',
+}).then(function(components){
+
+    pb1 = new components.ProgressBar({
             el: '#container1',
-            data:function(){return new ProgressBarVO(max,progress);}
-        });
-        
-        runTests();
-        
-        resizeTest();
-        
+            data:function(){return new ProgressBarVO(max,progress);} 
     });
+
+});
+
+function increment(value){
+    pb1.increase(value);
+    return pb1.usage;
+}
+
+function decrement(value){
+    pb1.decrease(value);
+    return pb1.usage;
+}
+
+function scale(value){
+    return pb1.scale(value);
+}
+
+function getUsagePercentage(){
+    return pb1.getUsagePercentage(pb1.usage);
+}
+
+function runTests(){
+    var timeoutInterval = 5000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     
-    function increment(value){
-        pb1.increase(value);
-        return pb1.usage;
-    }
+    describe('Progress Bar tests =>', function(){
 
-    function decrement(value){
-        pb1.decrease(value);
-        return pb1.usage;
-    }
+        describe( "Scale test :", function () {
+            beforeEach(function(done) {
+                setTimeout(function() {
+                    done();
+                },timeoutInterval);    
+            });
+            it("Converts percentage value to pixel value", function (done) {
+                  expect(scale(max)).toEqual(pb1.comp.clientWidth);  
+                  done();
+            });
+        }),
 
-    function scale(value){
-        return pb1.scale(value);
-    }
+        describe( "Incrementer test :", function () {
+            var incValue = 40;
+            var finalValue = 0;
 
-    function getUsagePercentage(){
-        return pb1.getUsagePercentage(pb1.usage);
-    }
-
-    function runTests(){
-        describe('Progress Bar tests =>', function(){
-    
-            describe( "Scale test :", function () {
-                beforeEach(function(done) {
-                 done();
-                });
-                it("Converts percentage value to pixel value", function (done) {
-                    expect(scale(max)).toEqual(parseInt(width));
-                });
-            }),
-
-            describe( "Incrementer test :", function () {
-                var incValue = 40;
-                var finalValue = 0;
-                beforeEach(function(done) {
-                 done();
-                });
-                it("increments progress bar to specified percentage", function () {
+            it("increments progress bar to specified percentage", function () {
                     pb1.reset();
 
                     finalValue += incValue;
                     expect(increment(incValue)).toEqual(scale(finalValue));
                     finalValue += incValue;
                     expect(increment(incValue)).toEqual(scale(finalValue));
-                });
-            }),
+            });
+        }),
 
-            describe( "Decrementer test :", function () {
-                var decValue = 40;
-                var finalValue = 80;
-                beforeEach(function(done) {
-                 done();
-                });
-                it("decrements progress bar to specified percentage", function () {
+        describe( "Decrementer test :", function () {
+            var decValue = 40;
+            var finalValue = 80;
+
+            it("decrements progress bar to specified percentage", function () {
                     pb1.reset();
 
                     increment(finalValue);
@@ -86,29 +85,30 @@
                     expect(decrement(decValue)).toEqual(scale(finalValue));
                     finalValue -= decValue;
                     expect(decrement(decValue)).toEqual(scale(finalValue));
-                });
-                it("progress bar percentage should not be less than zero", function () {
+            });
+            it("progress bar percentage should not be less than zero", function () {
                     expect(decrement(decValue)).toEqual(scale(0));
-                });
-            }),
+            });
+        }),
 
-            describe( "Progress Percentage Test :", function () {
-                beforeEach(function(done) {
-                 done();
-                });
-                it("display the progress in percentage", function () {
+        describe( "Progress Percentage Test :", function () {
+
+            it("display the progress in percentage", function () {
                     pb1.reset();
                     increment(40);
                     expect(getUsagePercentage()).toEqual(40);
-                });
-            })
-        
-        });
-    }
+            });
+        })
+    });   
+}
 
-    function resizeTest(){
+function resizeTest(){
         window.resizeTo(300, 600);
         describe('Progress Bar tests after resizing window =>', function(){
             runTests();
         });
-    };
+};
+
+runTests();
+
+resizeTest();
